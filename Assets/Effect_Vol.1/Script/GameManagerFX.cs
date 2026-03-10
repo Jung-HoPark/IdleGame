387a03afb8038cd78dfec9a1653d620c56b42469
Jung-HoPark <dumbvelvet71@gmail.com>
2026-03-10 11:14:52 +0900

    Feat
    에셋스토어 에셋들 추가.
    7.InternalAsset 및 Effect_Vol.1, Layer Lab 에 있습니다.

---
+++ Assets/Effect_Vol.1/Script/GameManagerFX.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerFX : MonoBehaviour
+{
+
+    public GameObject[] Setact;
+
+    public void setAcc(int num)
+    {
+        for (int i = 0; i < Setact.Length; i++)
+        {
+            Setact[i].SetActive(false);
+        }
+
+        Setact[num].SetActive(true);
+
+    }
+}
