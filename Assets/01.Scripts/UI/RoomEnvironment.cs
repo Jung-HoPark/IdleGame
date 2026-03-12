c3b90889cf0b55e11e4d0d8f8f139a065d543433
Teca <taewon4261@gmail.com>
2026-03-12 12:50:39 +0900

    feat: 방 환경(RoomEnvironment) 시스템 세팅
    기획서 기반 RoomEnvironment.cs 생성 및 단계별 방 진화(SpriteRenderer) 인스펙터 세팅 완료

--- Assets/01.Scripts/UI/RoomEnvironment.cs
+++ Assets/01.Scripts/UI/RoomEnvironment.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomEnvironment : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("방 업그레이드 단계별 이미지")]
+    public Sprite[] roomLevelSprites;
 
-    // Update is called once per frame
-    void Update()
+    [Header("연결할 배경 오브젝트")]
+    public SpriteRenderer roomSpriteRenderer;
+
+    public void ChangeRoomLevel(int level)
     {
+        // 에러 방지용
+        if (level < 0 || level >= roomLevelSprites.Length)
+        {
+            Debug.LogWarning($"{level}레벨에 해당하는 방 이미지가 없음");
+            return;
+        }
         
+        //여기서 스프라이트 교체
+        roomSpriteRenderer.sprite = roomLevelSprites[level];
     }
 }
